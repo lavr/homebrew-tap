814c8f4111662a68c1a6256b7e71cc9b98d4df2f
Sergey Lavrinenko <s.lavrinenko@gmail.com>
2026-03-13 20:55:36 +0000

    express-bot 0.6.0

--- Formula/express-bot.rb
+++ Formula/express-bot.rb
@@ -1,26 +1,26 @@
 class ExpressBot < Formula
   desc "CLI and HTTP server for sending messages to eXpress"
   homepage "https://github.com/lavr/express-bot"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lavr/express-bot/releases/download/0.5.0/express-bot-darwin-arm64.tar.gz"
-      sha256 "852e4acbdc5b8849eab369d9f6e13e3c3ab946218607825e27351c4ba4ca8e40"
+      url "https://github.com/lavr/express-bot/releases/download/0.6.0/express-bot-darwin-arm64.tar.gz"
+      sha256 "bd71dabc689c7de485c84357baec86963773bdc502ea1d895f1a2dd6bb4c1de8"
     else
-      url "https://github.com/lavr/express-bot/releases/download/0.5.0/express-bot-darwin-amd64.tar.gz"
-      sha256 "44339a1638a431f517023e26779b5f3bc67e9d9f0440ebff7df3eafce735ddce"
+      url "https://github.com/lavr/express-bot/releases/download/0.6.0/express-bot-darwin-amd64.tar.gz"
+      sha256 "2632671581da35346fb609321eb18c44cc1ae38db25d8f465aa343324d7f956e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lavr/express-bot/releases/download/0.5.0/express-bot-linux-arm64.tar.gz"
-      sha256 "753aa5aaa1b3748c51c6b26abada6c7732ea2ac523bc538b6954c76aa2b2d04b"
+      url "https://github.com/lavr/express-bot/releases/download/0.6.0/express-bot-linux-arm64.tar.gz"
+      sha256 "4908de9ea5a566c3f1e2c8281102045d8f91d52124b67f5122fb147f0cdc17fc"
     else
-      url "https://github.com/lavr/express-bot/releases/download/0.5.0/express-bot-linux-amd64.tar.gz"
-      sha256 "dedd94dc197f642d23d7d013138c3535c4e3956d406e2fe6c5312c1a8d9a681e"
+      url "https://github.com/lavr/express-bot/releases/download/0.6.0/express-bot-linux-amd64.tar.gz"
+      sha256 "f406bcb5a562dc3454fb598959c5e933cba78623a0b8ada6b65fdb9cbde95fae"
     end
   end
 
