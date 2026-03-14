7f2cfa77e08edaa1db76d2a87f10418ce7dd28b0
Sergey Lavrinenko <s.lavrinenko@gmail.com>
2026-03-14 14:59:08 +0000

    express-botx 0.14.0

--- Formula/express-botx.rb
+++ Formula/express-botx.rb
@@ -1,26 +1,26 @@
 class ExpressBotx < Formula
   desc "CLI and HTTP server for sending messages to eXpress"
   homepage "https://github.com/lavr/express-botx"
-  version "0.13.0"
+  version "0.14.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lavr/express-botx/releases/download/0.13.0/express-botx-darwin-arm64.tar.gz"
-      sha256 "c1a858b49b24186cb24d9e6c450441fb27ae11ebf6f0d05faf2bf2cb61444a48"
+      url "https://github.com/lavr/express-botx/releases/download/0.14.0/express-botx-darwin-arm64.tar.gz"
+      sha256 "78591135f48df90599ccfc17a52b28c031b154d0e6f2de1c826ef778b4770bd8"
     else
-      url "https://github.com/lavr/express-botx/releases/download/0.13.0/express-botx-darwin-amd64.tar.gz"
-      sha256 "c69e6fdafb071f9b143c0cf91fb1681344abd44b1fdc5202e99f60996468dd01"
+      url "https://github.com/lavr/express-botx/releases/download/0.14.0/express-botx-darwin-amd64.tar.gz"
+      sha256 "6b16fb2f81a9afed73d54c88adb18dee69cac091dc540163cd37945e371509e1"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lavr/express-botx/releases/download/0.13.0/express-botx-linux-arm64.tar.gz"
-      sha256 "b2e81dc5046a05810530578b9a52156ebee830701de4575c862610690177ec92"
+      url "https://github.com/lavr/express-botx/releases/download/0.14.0/express-botx-linux-arm64.tar.gz"
+      sha256 "935a76a1f3973a6e900908ea498f77833b3a7cfa527dc6d5675a744762162628"
     else
-      url "https://github.com/lavr/express-botx/releases/download/0.13.0/express-botx-linux-amd64.tar.gz"
-      sha256 "eb7b70a952f39d72e240c735a46307dd0f820fc200ea1d31468b0edea1bd37cc"
+      url "https://github.com/lavr/express-botx/releases/download/0.14.0/express-botx-linux-amd64.tar.gz"
+      sha256 "102c1b0f09f510c533073880a21138d1f48271a9c6ccaf736adaf9285d80855a"
     end
   end
 
