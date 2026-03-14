e3e9bf77b57cac2c6300a1c460f194eb31d65902
Sergey Lavrinenko <s.lavrinenko@gmail.com>
2026-03-14 13:22:56 +0000

    express-botx 0.10.0

--- Formula/express-botx.rb
+++ Formula/express-botx.rb
@@ -1,26 +1,26 @@
 class ExpressBotx < Formula
   desc "CLI and HTTP server for sending messages to eXpress"
   homepage "https://github.com/lavr/express-botx"
-  version "0.9.0"
+  version "0.10.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lavr/express-botx/releases/download/0.9.0/express-botx-darwin-arm64.tar.gz"
-      sha256 "f0f3472debcad665f9b3848125d45a909297888712b1d82053cd0bc7ee11e3f8"
+      url "https://github.com/lavr/express-botx/releases/download/0.10.0/express-botx-darwin-arm64.tar.gz"
+      sha256 "93129e993fe520125cdf2b06858c9678f4cd0376cb66c68c14960d0442c02c7e"
     else
-      url "https://github.com/lavr/express-botx/releases/download/0.9.0/express-botx-darwin-amd64.tar.gz"
-      sha256 "7aab015f547c861a1acecb81b5e0dd92c63bccf86cbc39e2d86dddd43e8deb0f"
+      url "https://github.com/lavr/express-botx/releases/download/0.10.0/express-botx-darwin-amd64.tar.gz"
+      sha256 "a9677e4f0285466681f4f2197ab6e025759438e971a2f5369b80c86c1ad091e0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lavr/express-botx/releases/download/0.9.0/express-botx-linux-arm64.tar.gz"
-      sha256 "fd5a834c1cc28dd616f1c59bcc9059cdb9137e4ac496ac073cfc12fd8aa104e0"
+      url "https://github.com/lavr/express-botx/releases/download/0.10.0/express-botx-linux-arm64.tar.gz"
+      sha256 "becd197c00dc08f76501c8b5c69f2ac43f1069f6987cac9b084d474dfee1baef"
     else
-      url "https://github.com/lavr/express-botx/releases/download/0.9.0/express-botx-linux-amd64.tar.gz"
-      sha256 "865d18bd7b64907903ecd174d9d97044242cb9f21bb0f69d86c66467c250aeea"
+      url "https://github.com/lavr/express-botx/releases/download/0.10.0/express-botx-linux-amd64.tar.gz"
+      sha256 "a6c9fdc17477e16afc885c5804684e6ecd663f223e9bfc038878b3eda8049a15"
     end
   end
 
